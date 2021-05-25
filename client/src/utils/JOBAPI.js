dc62e3e964471391aee9faabde39823d8d3acc5a
Mena Wahba <menawahba@gmail.com>
2021-05-25 15:57:27 -0400

    fixed api issiue

--- client/src/utils/JOBAPI.js
+++ client/src/utils/JOBAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://localhost:3001/api/jobs";
+const url = "/api/jobs";
 
 const jobs = {
     search: function () {
--- client/src/utils/interviewAPI.js
+++ client/src/utils/interviewAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://localhost:3001/api/interviews";
+const url = "/api/interviews";
 
 const interview = {
     saveInterview: function (interview) {
